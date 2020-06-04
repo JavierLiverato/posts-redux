55e4457669b85d17642355e6d0c23c78f0f573bd
Javier <Liverato>
2020-06-04 11:53:45 -0500

    added production API root

--- src/config/config.js
+++ src/config/config.js
@@ -5,8 +5,8 @@ export const APP_NAME = process.env.name || 'TEST';
 export const TOKEN_PREFIX = 'Token ';
 
 export const API_ENDPOINT = process.env.NODE_ENV === 'production'
-    ? 'http://localhost:4006/api/v1' :
-    'http://localhost:4006/api/v1';
+    ? 'https://test-posts.herokuapp.com/api/v1' :
+    'https://test-posts.herokuapp.com/api/v1';
 
 export const defaultResponses = {
     noResponseFromApi: 'No hay respuesta por parte del servidor. Por favor intente de nuevo más tarde.'
